9c8b3e0e30b1a802550e6ddb7cf8fe10292ac1b3
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2023-08-22 20:56:09 +0300

    a

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useEffect } from 'react';
+
 import Header from './components/Header/Header';
+import { useTelegram } from './hooks/useTelegram';
 
 function App() {
+  const { tg, onToggleButton } = useTelegram();
+
+  useEffect(() => {
+    tg.ready();
+  }, [tg]);
+
   return (
     <div className="App">
       <Header />
+      <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,16 @@
-import { useEffect } from 'react';
+import { useTelegram } from '../../hooks/useTelegram';
 
 import Button from '../Button/Button';
+
 import styles from './Header.module.css';
 
-const tg = window.Telegram.WebApp;
-
 const Header = () => {
-  useEffect(() => {
-    tg.ready();
-  }, []);
-
-  const onClose = () => {
-    tg.close();
-  };
+  const { user, onClose } = useTelegram();
 
   return (
     <div className={styles.header}>
       <Button onClick={onClose}>Close</Button>
-      <span className={styles.username}>
-        {tg.initDataUnsafe?.user?.username}
-      </span>
+      <span className={styles.username}>{user?.username}</span>
     </div>
   );
 };
